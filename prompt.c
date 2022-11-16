e64184d8d7c78f40b4f96b6fa543504cb646b2b0
Florence Coker Damilola <damselpiccolo18@gmail.com>
2022-11-16 05:10:55 +0100

    simple_shell

--- prompt.c
+++ prompt.c
@@ -1,98 +1,46 @@
 #include "main.h"
-
 /**
- * lookforslash - identifies if first char is a slash.
- * @cmd: first string
- * Return: 1 if yes 0 if no.
- */
-int lookforslash(char *cmd)
+ * prompt - call prompt from another function (prompt)
+ **/
+void prompt(void)
 {
-	int cont = 0;
-
-	while (cmd[cont])
+	for (;;)
 	{
-		if (cmd[0] == '/')
+		char *text = NULL, **environ;
+		pid_t child_pid;
+		int status, byte_read;
+		size_t bufsize = 0;
+
+		place("$ ");
+		byte_read = getline(&text, &bufsize, stdin);
+		if (byte_read == -1)
+			exit(98);
+		if (compareExit(text, "exit") == 0)
+			exit(0);
+		if (compareEnv(text, "env") == 0)
 		{
-			printf("%c\n", cmd[0]);
-			return (1);
-		}
-
-		cont++;
-	}
-	return (0);
-}
-
-/**
- * compareExit - Checks if the user is typed exit
- * @s1: first string
- * @s2: exit string
- * Return: 1 if yes 0 if no.
- */
-int compareExit(char *s1, char *s2)
-{
-	int i = 0;
-
-	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-	{
-		if (i == 3)
-			break;
-		i++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
-}
-
-/**
- * compareEnv - checks if the used typed env.
- * @s1: first string
- * @s2: exit string
- * Return: 1 if yes 0 if no.
- */
-int compareEnv(char *s1, char *s2)
-{
-	int i = 0;
-
-	for (; (*s2 != '\0' && *s1 != '\0') && *s1 == *s2; s1++)
-	{
-		if (i == 2)
-			break;
-		i++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
-}
-/**
- * identify_string - identyfy keyboard input.
- * @parameter: call prompt from another function (prompt)
- * Return: str
- **/
-char **identify_string(char *parameter)
-{
-	char **buf = malloc(1024 * sizeof(char *));
-	char *split;
-	int i = 0;
-	char *delim = " \t\n";
-
-
-	split = strtok(parameter, delim);
-
-	while (split != NULL)
-	{
-		buf[i] = split;
-		i++;
-		split = strtok(NULL, delim);
-	}
-	execute_proc(buf);
-	return (buf);
-}
-/**
- * controlC - avoid close the shell
- * @sig: keep going shell
- **/
-void  controlC(int sig)
-{
-	(void) sig;
-	write(1, "\n$ ", 3);
-}
+			while (*environ != NULL)
+			{
+				if (!(_strcmpdir(*environ, "USER")) ||
+						!(_strcmpdir(*environ, "LANGUAGE")) ||
+						!(_strcmpdir(*environ, "SESSION")) ||
+						!(_strcmpdir(*environ, "COMPIZ_CONFIG_PROFILE")) ||
+						!(_strcmpdir(*environ, "SHLV")) ||
+						!(_strcmpdir(*environ, "HOME")) ||
+						!(_strcmpdir(*environ, "C_IS")) ||
+						!(_strcmpdir(*environ, "DESKTOP_SESSION")) ||
+						!(_strcmpdir(*environ, "LOGNAME")) ||
+						!(_strcmpdir(*environ, "TERM")) ||
+						!(_strcmpdir(*environ, "PATH")))
+				{
+					place(*environ), place("\n");
+				}
+				environ++;
+			}}
+		child_pid = fork();
+		if (child_pid < 0)
+			perror("Error");
+		if (child_pid == 0)
+			identify_string(text);
+		else
+			wait(&status); }}
